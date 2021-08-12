2785fc6492e030de011fb6c1284e86773302e75d
Luiz Miguel <luizmiguelproenca@gmail.com>
2021-08-12 13:41:48 -0300

    phase 4

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -9,9 +9,10 @@ namespace DIO.Series
         private string Titulo { get; set; }
         private string Descricao { get; set; }
         private int Ano { get; set; }
+
+        private bool Excluido{ get; set;}
         
         // Métodos
-
         public Serie(int id, Genero genero, string titulo, string descricao, int ano)
         {
             this.Id = id;
@@ -19,6 +20,7 @@ namespace DIO.Series
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+            this.Excluido = false;
         }
 
         public override string ToString()
@@ -38,6 +40,13 @@ namespace DIO.Series
         public int retornaId()
         {
             return this.Id;
+        }
+        public bool retornaExcluido()
+		{
+			return this.Excluido;
+		}
+        public void Excluir(){
+            this.Excluido = true;
         }
 
     }
